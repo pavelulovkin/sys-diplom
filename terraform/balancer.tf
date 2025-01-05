22145626b41fae532d2eb1348e708d3da41792d0
Pavel Ulovkin <ulovkin@gmail.com>
2025-01-06 02:13:29 +0300

    load-balancer

--- terraform/balancer.tf
+++ terraform/balancer.tf
@@ -1,10 +1,10 @@
 resource "yandex_alb_target_group" "tg_web-servers" {
   name = "tg-web-servers"
   dynamic "target" {
-    for_each = yandex_compute_instance.web-server
+    for_each          = yandex_compute_instance.web-server
     content {
-      subnet_id  = yandex_vpc_subnet.subnet_private_web[target.key].id
-      ip_address = target.value.network_interface[0].ip_address
+      subnet_id       = yandex_vpc_subnet.subnet_private_web[target.key].id
+      ip_address      = target.value.network_interface[0].ip_address
     }
   }
 }
@@ -12,20 +12,64 @@ resource "yandex_alb_target_group" "tg_web-servers" {
 resource "yandex_alb_backend_group" "backend_group" {
   name = "backend-group"
   http_backend {
-    name             = "backend-web"
-    port             = 80
-    target_group_ids = [yandex_alb_target_group.tg_web-servers.id]
-    load_balancing_config {
-      panic_threshold = 80
-    }
+    name              = "backend-web"
+    port              = 80
+    target_group_ids  = [yandex_alb_target_group.tg_web-servers.id]
     healthcheck {
+      timeout         = "5s"
+      interval        = "60s"
       http_healthcheck {
-        path = "/"
+        path          = "/"
       }
     }
   }
 }
 
 resource "yandex_alb_http_router" "router_web" {
-  name = "router-web"
+  name                = "router-web"
+}
+
+resource "yandex_alb_virtual_host" "virtual_host_web" {
+  name                = "virtual-host-web"
+  http_router_id      = yandex_alb_http_router.router_web.id
+  route {
+    name = "route-main"
+    http_route {
+      http_match {
+        path {
+          prefix = "/"
+        }
+      }
+      http_route_action {
+        backend_group_id = yandex_alb_backend_group.backend_group.id
+        timeout          = "5s"
+      }
+    }
+  }
+}
+
+resource "yandex_alb_load_balancer" "load_balancer_web" {
+  name               = "load-balancer-web"
+  network_id         = yandex_vpc_network.network_cloud.id
+  security_group_ids = [yandex_vpc_security_group.rules_load_balancer.id, yandex_vpc_security_group.rules_internal.id] 
+  allocation_policy {
+    location {
+      zone_id   = var.YC_ZONES[0]
+      subnet_id = yandex_vpc_subnet.subnet_public.id
+    }
+  }
+  listener {
+    name = "listener-web"
+    endpoint {
+      address {
+        external_ipv4_address {}
+      }
+      ports = [80]
+    }
+    http {
+      handler {
+        http_router_id = yandex_alb_http_router.router_web.id
+      }
+    }
+  }
 }
--- terraform/security.tf
+++ terraform/security.tf
@@ -1,1 +1,37 @@
 #...
+resource "yandex_vpc_security_group" "rules_load_balancer" {
+  name       = "rules-load-balancer"
+  network_id = yandex_vpc_network.network_cloud.id
+
+  ingress {
+    protocol       = "tcp"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 80
+  }
+
+  ingress {
+    protocol       = "icmp"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol       = "any"
+    description    = "allow any outgoing connection"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "yandex_vpc_security_group" "rules_internal" {
+  name       = "rules-internal"
+  network_id = yandex_vpc_network.network_cloud.id
+
+  ingress {
+    protocol       = "any"
+    v4_cidr_blocks = ["10.0.0.0/24", "10.1.0.0/24", "10.2.0.0/16"]
+  }
+
+  egress {
+    protocol       = "any"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
