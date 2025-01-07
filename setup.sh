36cd77ae5b8f090f9a7694a185573f23fb24e146
Pavel Ulovkin <ulovkin@gmail.com>
2025-01-08 02:20:27 +0300

    zabbix-server...

--- setup.sh
+++ setup.sh
@@ -3,37 +3,26 @@
 ROOT_DIR="/mnt/x/important/sys-diplom"
 
 cd $ROOT_DIR/terraform
+terraform plan
 terraform apply -auto-approve
 
 BASTION_IP=$(terraform output -raw bastion_ip)
+ZABBIX_INTERNAL_IP=$(terraform output -raw zabbix_internal_ip)
 
 if [ -z "$BASTION_IP" ]; then
   echo "Не удалось получить внешний IP хоста Bastion"
   exit 1
 fi
+if [ -z "$ZABBIX_INTERNAL_IP" ]; then
+  echo "Не удалось получить внутренний IP zabbix-server"
+  exit 1
+fi
 
 cd $ROOT_DIR/ansible
-cat > inventory.ini <<EOL
-[bastion-proxy]
-bastion.ru-central1.internal ansible_host=$BASTION_IP
+sed "s/BASTION_IP/$BASTION_IP/g" ./templates/inventory_template.ini > ./inventory.ini
+sed -i "s/^zabbix_internal_ip:.*/zabbix_internal_ip: \"$ZABBIX_INTERNAL_IP\"/" ./private/vars.yml
 
-[bastion-proxy:vars]
-ansible_user=external
-ansible_ssh_private_key_file=../terraform/private/external
-ansible_ssh_extra_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+export ANSIBLE_CONFIG="./ansible.cfg"
 
-[internal]
-web-server-1.ru-central1.internal
-web-server-2.ru-central1.internal
-zabbix.ru-central1.internal
-kibana.ru-central1.internal
-elastic.ru-central1.internal
-
-[internal:vars]
-ansible_ssh_common_args='-o ProxyCommand="ssh -i ../terraform/private/external -W %h:%p external@$BASTION_IP"'
-ansible_user=internal
-ansible_ssh_private_key_file=../terraform/private/internal
-ansible_ssh_extra_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
-EOL
-
-ansible-playbook -i ./inventory.ini ./playbooks/ping.yml -e ansible_remote_tmp=/tmp/.ansible/tmp
+# ansible-playbook -i ./inventory.ini ./playbooks/ping.yml -e ansible_remote_tmp=/tmp/.ansible/tmp
+ansible-playbook -i ./inventory.ini ./playbooks/zabbix-server.yml -e ansible_remote_tmp=/tmp/.ansible/tmp
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "bastion_ip" {
   value = yandex_compute_instance.bastion.network_interface[0].nat_ip_address
 }
+output "zabbix_internal_ip" {
+  value = yandex_compute_instance.zabbix.network_interface[0].ip_address
+}
