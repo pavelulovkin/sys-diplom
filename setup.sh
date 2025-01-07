56bd5f2da1ac84c3d8d575a98d244e269775a550
Pavel Ulovkin <ulovkin@gmail.com>
2025-01-07 20:14:25 +0300

    proxy-jump работает (тут нецензурное выражение)

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+ROOT_DIR="/mnt/x/important/sys-diplom"
+
+cd $ROOT_DIR/terraform
+terraform apply -auto-approve
+
+BASTION_IP=$(terraform output -raw bastion_ip)
+
+if [ -z "$BASTION_IP" ]; then
+  echo "Не удалось получить внешний IP хоста Bastion"
+  exit 1
+fi
+
+# export ANSIBLE_SSH_ARGS="-o ProxyCommand='ssh -W %h:%p -q external@$BASTION_IP' "
+
+cd $ROOT_DIR/ansible
+cat > inventory.ini <<EOL
+[bastion-proxy]
+$BASTION_IP
+
+[bastion-proxy:vars]
+ansible_user=external
+ansible_ssh_private_key_file=../terraform/private/external
+ansible_ssh_extra_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+
+[internal]
+web-server-1.ru-central1.internal
+web-server-2.ru-central1.internal
+zabbix.ru-central1.internal
+kibana.ru-central1.internal
+elastic.ru-central1.internal
+
+[internal:vars]
+ansible_ssh_common_args='-o ProxyCommand="ssh -i ../terraform/private/external -W %h:%p external@$BASTION_IP"'
+ansible_user=internal
+ansible_ssh_private_key_file=../terraform/private/internal
+ansible_ssh_extra_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+EOL
+
+ansible-playbook -i ./inventory.ini ./playbooks/ping.yml -e ansible_remote_tmp=/tmp/.ansible/tmp
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,3 @@
-output "network_id" {
-  value = yandex_vpc_network.network_cloud.id
-}
-
-output "subnet_public_id" {
-  value = yandex_vpc_subnet.subnet_public.id
-}
-
-output "subnet_private_web_0_id" {
-  value = yandex_vpc_subnet.subnet_private_web[0].id
-}
-
-output "subnet_private_web_1_id" {
-  value = yandex_vpc_subnet.subnet_private_web[1].id
-}
-
-output "subnet_private_elastic_id" {
-  value = yandex_vpc_subnet.subnet_private_elastic.id
+output "bastion_ip" {
+  value = yandex_compute_instance.bastion.network_interface[0].nat_ip_address
 }
