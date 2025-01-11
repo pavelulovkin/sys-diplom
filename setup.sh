530ecec77a1a3172f785097b4fb344f38b91054f
Pavel Ulovkin <ulovkin@gmail.com>
2025-01-11 19:43:46 +0300

    zabbix-api...

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+if ansible-galaxy collection list | grep -q 'community.zabbix'; then
+  echo "Коллекция community.zabbix установлена."
+else
+  ansible-galaxy collection install community.zabbix
+fi
+
 
 ROOT_DIR="/mnt/x/important/sys-diplom"
 API_KEY="85z9s2uP1Zx3H8JY7xIsa611AT3xFo"
@@ -21,26 +27,26 @@ if [ -z "$ZABBIX_INTERNAL_IP" ]; then
   exit 1
 fi
 
-dns_records['sys34-ulovkinp.run.place']=$BALANCER_IP
-dns_records['zabbix.sys34-ulovkinp.run.place']=$BASTION_IP
-dns_records['kibana.sys34-ulovkinp.run.place']=$BASTION_IP
+# dns_records['sys34-ulovkinp.run.place']=$BALANCER_IP
+# dns_records['zabbix.sys34-ulovkinp.run.place']=$BASTION_IP
+# dns_records['kibana.sys34-ulovkinp.run.place']=$BASTION_IP
 
-cd $ROOT_DIR
-for NAME in "${!dns_records[@]}"; do
-    IP="${dns_records[$NAME]}"
-    cat <<EOF > ./update.json
-{
-    "update": {
-        "type": "A",
-        "name": "$NAME",
-        "content": "$IP",
-        "ttl": 5
-    }
-}
-EOF
-    cat ./update.json
-    curl "https://api.dnsexit.com/dns/ud/?apikey=$API_KEY&host=$NAME&ip=$IP"
-done
+# cd $ROOT_DIR
+# for NAME in "${!dns_records[@]}"; do
+#     IP="${dns_records[$NAME]}"
+#     cat <<EOF > ./update.json
+# {
+#     "update": {
+#         "type": "A",
+#         "name": "$NAME",
+#         "content": "$IP",
+#         "ttl": 5
+#     }
+# }
+# EOF
+#     cat ./update.json
+#     curl "https://api.dnsexit.com/dns/ud/?apikey=$API_KEY&host=$NAME&ip=$IP"
+# done
 
 cd $ROOT_DIR/ansible
 sed "s/BASTION_IP/$BASTION_IP/g" ./templates/inventory_template.ini > ./inventory.ini
