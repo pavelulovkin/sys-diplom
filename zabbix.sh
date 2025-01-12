9903152e5b9fa35216672ade3f1afbda8475663f
Pavel Ulovkin <ulovkin@gmail.com>
2025-01-12 19:37:18 +0300

    its only getting worse

--- zabbix.sh
+++ zabbix.sh
@@ -1,14 +1,14 @@
-# curl -X POST "https://zabbix.sys34-ulovkinp.run.place/api_jsonrpc.php" \
-# -H "Content-Type: application/json" \
-# -d '{
-#     "jsonrpc": "2.0",
-#     "method": "user.login",
-#     "params": {
-#         "username": "Admin",
-#         "password": "zabbix"
-#     },
-#     "id": 1
-# }'
+curl -X POST "https://zabbix.sys34-ulovkinp.run.place/api_jsonrpc.php" \
+-H "Content-Type: application/json" \
+-d '{
+    "jsonrpc": "2.0",
+    "method": "user.login",
+    "params": {
+        "username": "Admin",
+        "password": "123456"
+    },
+    "id": 1
+}'
 
 curl -X POST "https://zabbix.sys34-ulovkinp.run.place/api_jsonrpc.php" \
 -H "Content-Type: application/json" -H "Authorization: Bearer 4e5ce135eb1bdd9e1e02b85b7e8ac653" \
